9e00d91c81be037d0e9af69e12fa4bc87c411fef
ashleynicole472 <asha.4720@gmail.com>
2018-06-27 11:46:11 -0700

    Created components, adding skeleton outline to each

---
+++ my_profile/src/Components/AboutMe/AboutMe.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './AboutMe.css';
+
+class AboutMe extends Component {
+    render(){
+        return (
+        )
+    }
+}
+
+export default AboutMe;
