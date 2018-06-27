34badfe888f3d24021de310a1adf8e8b62388bfc
ashleynicole472 <asha.4720@gmail.com>
2018-06-27 11:57:20 -0700

    Added Navbar component

--- my_profile/src/App.js
+++ my_profile/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import NavBar from './Components/NavBar/NavBar.js';
 import AboutMe from './Components/AboutMe/AboutMe.js';
 import Contact from './Components/Contact/Contact.js';
 import Footer from './Components/Footer/Footer.js';
@@ -7,7 +8,8 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-        <div className="App">
+        <div>
+            <NavBar />
             <AboutMe />
             <Contact />
             <Footer />
---
+++ my_profile/src/Components/NavBar/NavBar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './NavBar.css';
+
+class NavBar extends Component {
+    render(){
+        return(
+            <h2>Hey! Im the Navigation bar</h2>
+        )
+    }
+}
+export default NavBar;
