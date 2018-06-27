ee3f5849209715ee1a815285c54d6323e722a668
ashleynicole472 <asha.4720@gmail.com>
2018-06-27 11:53:48 -0700

    Added footer component

--- my_profile/src/App.js
+++ my_profile/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import AboutMe from './Components/AboutMe/AboutMe.js';
 import Contact from './Components/Contact/Contact.js';
+import Footer from './Components/Footer/Footer.js';
 import './App.css';
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
         <div className="App">
             <AboutMe />
             <Contact />
+            <Footer />
         </div>
     );
   }
---
+++ my_profile/src/Components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Footer.css';
+
+class Footer extends Component {
+    render(){
+        return(
+            <h6>I am the footer</h6>
+        )
+    }
+}
+
+export default Footer;
