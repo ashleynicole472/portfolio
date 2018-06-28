3750b4a4ca666e523a4ee811f5207df9a423b7e1
ashleynicole472 <asha.4720@gmail.com>
2018-06-28 11:33:34 -0700

    Added hover overlay effect on project sqaures

--- my_profile/src/Components/Projects/Projects.js
+++ my_profile/src/Components/Projects/Projects.js
@@ -7,90 +7,138 @@ class Projects extends Component {
             <div className="projects-container">
                 <div className="row w3-center w3-animate-bottom">
                     <div className="col-md-12">
-                        <h1>My Projects</h1> <h5> View more projects on my <a href="http://www.github.com/ashleynicole472"><strong>GitHub</strong><i className="fab fa-github"></i></a></h5>
+                        <h1>My Projects</h1> <h5> View more projects on my <a href="http://www.github.com/ashleynicole472" target="#"><strong>GitHub</strong><i className="fab fa-github"></i></a></h5>
                         <hr />
                     </div>
                 </div>
                 <div className="row cards-center w3-center w3-animate-bottom">
                     <div className="col-sm-3">
-                        <div className="card border-light mb-3">
-                            <div className="card-header">Project
+                        <div className="container">
+                            <div className="card border-light mb-3">
+                                <div className="card-header">Project
+                                </div>
+                                <div className="card-body">
+                                    <h4 className="card-title">Light card title</h4>
+                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+                                </div>
                             </div>
-                            <div className="card-body">
-                                <h4 className="card-title">Light card title</h4>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-sm-3">
-                        <div className="card border-primary mb-3">
-                            <div className="card-header">Header
-                            </div>
-                            <div className="card-body">
-                                <h4 className="card-title">Light card title</h4>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+                            <div className="overlay">
+                                <div className="text">My project
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div className="col-sm-3">
-                        <div className="card border-success mb-3">
-                            <div className="card-header">Header
+                        <div className="container">
+                            <div className="card border-primary mb-3">
+                                <div className="card-header">Header
+                                </div>
+                                <div className="card-body">
+                                    <h4 className="card-title">Light card title</h4>
+                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+                                </div>
                             </div>
-                            <div className="card-body">
-                                <h4 className="card-title">Light card title</h4>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+                            <div className="overlay">
+                                <div className="text">My project
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div className="col-sm-3">
-                        <div className="card border-danger mb-3">
-                            <div className="card-header">Header
+                        <div className="container">
+                            <div className="card border-success mb-3">
+                                <div className="card-header">Header
+                                </div>
+                                <div className="card-body">
+                                    <h4 className="card-title">Light card title</h4>
+                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+                                </div>
                             </div>
-                            <div className="card-body">
-                                <h4 className="card-title">Light card title</h4>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+                            <div className="overlay">
+                                <div className="text">My project
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-sm-3">
+                        <div className="container">
+                            <div className="card border-danger mb-3">
+                                <div className="card-header">Header
+                                </div>
+                                <div className="card-body">
+                                    <h4 className="card-title">Light card title</h4>
+                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+                                </div>
+                            </div>
+                            <div className="overlay">
+                                <div className="text">My project
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className="row cards-center w3-center w3-animate-bottom">
                     <div className="col-sm-3">
-                        <div className="card border-warning mb-3">
-                            <div className="card-header">Header
+                        <div className="container">
+                            <div className="card border-warning mb-3">
+                                <div className="card-header">Header
+                                </div>
+                                <div className="card-body">
+                                    <h4 className="card-title">Light card title</h4>
+                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+                                </div>
                             </div>
-                            <div className="card-body">
-                                <h4 className="card-title">Light card title</h4>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-sm-3">
-                        <div className="card border-info mb-3">
-                            <div className="card-header">Header
-                            </div>
-                            <div className="card-body">
-                                <h4 className="card-title">Light card title</h4>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+                            <div className="overlay">
+                                <div className="text">My project
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div className="col-sm-3">
-                        <div className="card border-light mb-3">
-                            <div className="card-header">Header
+                        <div className="container">
+                            <div className="card border-info mb-3">
+                                <div className="card-header">Header
+                                </div>
+                                <div className="card-body">
+                                    <h4 className="card-title">Light card title</h4>
+                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+                                </div>
                             </div>
-                            <div className="card-body">
-                                <h4 className="card-title">Light card title</h4>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+                            <div className="overlay">
+                                <div className="text">My project
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div className="col-sm-3">
-                        <div className="card border-primary mb-3">
-                            <div className="card-header">Header
+                        <div className="container">
+                            <div className="card border-light mb-3">
+                                <div className="card-header">Header
+                                </div>
+                                <div className="card-body">
+                                    <h4 className="card-title">Light card title</h4>
+                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+                                </div>
                             </div>
-                            <div className="card-body">
-                                <h4 className="card-title">Light card title</h4>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+                            <div className="overlay">
+                                <div className="text">My project
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-sm-3">
+                        <div className="container">
+                            <div className="card border-primary mb-3">
+                                <div className="card-header">Header
+                                </div>
+                                <div className="card-body">
+                                    <h4 className="card-title">Light card title</h4>
+                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+                                </div>
+                            </div>
+                            <div className="overlay">
+                                <div className="text">My project
+                                </div>
                             </div>
                         </div>
                     </div>
