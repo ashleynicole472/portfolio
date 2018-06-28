a0d4e32509a37642c231d6ab63ef679c7adf4c46
ashleynicole472 <asha.4720@gmail.com>
2018-06-28 11:38:48 -0700

    Fixed styling issue with overlay effect in project component

--- my_profile/src/Components/Projects/Projects.js
+++ my_profile/src/Components/Projects/Projects.js
@@ -15,8 +15,7 @@ class Projects extends Component {
                     <div className="col-sm-3">
                         <div className="container">
                             <div className="card border-light mb-3">
-                                <div className="card-header">Project
-                                </div>
+
                                 <div className="card-body">
                                     <h4 className="card-title">Light card title</h4>
                                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
@@ -31,8 +30,7 @@ class Projects extends Component {
                     <div className="col-sm-3">
                         <div className="container">
                             <div className="card border-primary mb-3">
-                                <div className="card-header">Header
-                                </div>
+
                                 <div className="card-body">
                                     <h4 className="card-title">Light card title</h4>
                                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
@@ -47,8 +45,7 @@ class Projects extends Component {
                     <div className="col-sm-3">
                         <div className="container">
                             <div className="card border-success mb-3">
-                                <div className="card-header">Header
-                                </div>
+
                                 <div className="card-body">
                                     <h4 className="card-title">Light card title</h4>
                                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
@@ -63,8 +60,7 @@ class Projects extends Component {
                     <div className="col-sm-3">
                         <div className="container">
                             <div className="card border-danger mb-3">
-                                <div className="card-header">Header
-                                </div>
+
                                 <div className="card-body">
                                     <h4 className="card-title">Light card title</h4>
                                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
@@ -81,8 +77,7 @@ class Projects extends Component {
                     <div className="col-sm-3">
                         <div className="container">
                             <div className="card border-warning mb-3">
-                                <div className="card-header">Header
-                                </div>
+
                                 <div className="card-body">
                                     <h4 className="card-title">Light card title</h4>
                                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
@@ -97,8 +92,7 @@ class Projects extends Component {
                     <div className="col-sm-3">
                         <div className="container">
                             <div className="card border-info mb-3">
-                                <div className="card-header">Header
-                                </div>
+
                                 <div className="card-body">
                                     <h4 className="card-title">Light card title</h4>
                                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
@@ -113,8 +107,7 @@ class Projects extends Component {
                     <div className="col-sm-3">
                         <div className="container">
                             <div className="card border-light mb-3">
-                                <div className="card-header">Header
-                                </div>
+
                                 <div className="card-body">
                                     <h4 className="card-title">Light card title</h4>
                                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
@@ -129,8 +122,7 @@ class Projects extends Component {
                     <div className="col-sm-3">
                         <div className="container">
                             <div className="card border-primary mb-3">
-                                <div className="card-header">Header
-                                </div>
+
                                 <div className="card-body">
                                     <h4 className="card-title">Light card title</h4>
                                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
